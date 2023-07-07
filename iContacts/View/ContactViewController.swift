f9ce998f22c80fd215816f04ff952e72f5f91936
Alibek <alibek.b1@icloud.com>
2023-07-07 18:23:43 +0600

    add contactViewController and build interface

---
+++ iContacts/View/ContactViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ContactViewController.swift
+//  iContacts
+//
+//  Created by Alibek Allamzharov on 07.07.2023.
+//
+
+import UIKit
+
+class ContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
