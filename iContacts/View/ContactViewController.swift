5ba6f56edb3a317da25dcecd939179e7b8948b31
Alibek Allamzharov <alkhero21@MBP-Alibek.lan>
2023-08-13 18:06:28 +0600

    add function editContact for ContactViewController

--- iContacts/View/ContactViewController.swift
+++ iContacts/View/ContactViewController.swift
@@ -24,6 +24,9 @@ class ContactViewController: UIViewController {
     
     var contact: Contact!
     let contactManager = ContactManager()
+    var timer: Timer?
+    var countDown: Int = 0
+    var countDownTotal: Int = 5
     
     
     override func viewDidLoad() {
@@ -99,5 +102,115 @@ class ContactViewController: UIViewController {
         contact = editedContact
         setupContact()
     }
+    
+    
+    @IBAction func deleteButtonTapped(_ sender: Any) {
+        
+        let alertController = UIAlertController(title: "Warning!", message: "Are you sure you want to delete this contact?", preferredStyle: .alert)
+        let deleteAction = UIAlertAction(title: "Delete", style: .destructive) { _ in
+            self.deleteContact()
+        }
+        alertController.addAction(deleteAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true)
+    }
+    
+    
+    @IBAction func undoDeleteButtonTapped(_ sender: Any) {
+        timer?.invalidate()
+        
+        progressView.progress = 1
+        progressView.isHidden = true
+        undoDeleteButton.isHidden = true
+        deleteContactButton.isHidden = false
+        
+        contactManager.add(contact: contact)
+    }
+    
+    
+    @IBAction func callButtonTapped(_ sender: Any) {
+        
+        open(contactType: .call)
+    }
+    
+    
+    @IBAction func faceTimeButtonTapped(_ sender: Any) {
+        open(contactType: .faceTime)
+    }
+    
+    @IBAction func messageButtonTapped(_ sender: Any) {
+        open(contactType: .message)
+    }
+    
+    
+    @IBAction func phoneNumberTapped(_ sender: Any) {
+        open(contactType: .call)
+    }
+    
+    func deleteContact() {
+        contactManager.delete(contactToDelete: self.contact)
+        deleteContactButton.isHidden = true
+        undoDeleteButton.isHidden = false
+        progressView.progress = 1
+        progressView.isHidden = false
+        
+        sheduleTimer()
+    }
+    
+    func sheduleTimer() {
+        countDown = countDownTotal
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateProgressView), userInfo: nil, repeats: true)
+    }
+    
+    @objc
+    func updateProgressView() {
+        countDown -= 1
+        progressView.progress = Float(countDown) / Float(countDownTotal)
+        
+        if countDown == 0 {
+            timer?.invalidate()
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    
+    func open(contactType: ContactType) {
+            
+        // Извлекается номер телефона
+        let phone = contact.phone
+        // Удаляются все символы '+' из сторки phone
+        let phoneWithoutPlus = phone.replacingOccurrences(of: "+", with: "")
+        // Удаляются все символы ' ' (пробелы) из сторки phoneWithoutPlus
+        let phoneWithoutSpacing = phoneWithoutPlus.replacingOccurrences(of: " ", with: "")
+        // Создается сторка которая указывает путь к приложению
+        let urlString: String = "\(contactType.urlScheme)" + phoneWithoutSpacing
+        
+        // Проверяется на преображение сторки urlString в URL
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        // Открывает приложение на iPhone/iPad с указанным путем. Например: "tel://77082968612" приложение контакты и позвонит на указанный номер
+        UIApplication.shared.open(url)
+    }
+}
 
+
+enum ContactType {
+    case message
+    case call
+    case faceTime
+    
+    var urlScheme: String {
+        switch self {
+        case .message:
+            return "sms://"
+        case .call:
+            return "tel://"
+        case .faceTime:
+            return "facetime://"
+        }
+    }
 }
